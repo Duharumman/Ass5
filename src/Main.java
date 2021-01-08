430b5f95451712a4f913ca8a56940d35a64c897c
Duha <duha.abu99@gmail.com>
2021-01-08 14:26:59 +0200

    add feature branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Master");
-        System.out.println("newfeature");
+        System.out.println("newfeature test");
     }
 }
